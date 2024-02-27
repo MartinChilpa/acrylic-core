02e3997ec4d76eb261ef36fe2a84bf22867215e2
Antonio Melé <antonio.mele@gmail.com>
2024-02-27 18:52:13 +0100

    fix

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,4 @@
 from psycogreen.gevent import patch_psycopg     # use this if you use gevent workers
-from psycogreen.eventlet import patch_psycopg   # use this if you use eventlet workers
 
 def post_fork(server, worker):
     patch_psycopg()
