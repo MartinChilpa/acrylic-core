5ef5938a5e9213a44a51656714f086942c58a564
Antonio Melé <antonio.mele@gmail.com>
2024-05-10 16:51:25 +0200

    fix command

--- chartmetric/management/commands/load_chartmetric_stats.py
+++ chartmetric/management/commands/load_chartmetric_stats.py
@@ -1,5 +1,4 @@
 from catalog.models import *
-from chartmetric.engine import Chartmetric
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from artist.models import Artist
@@ -11,8 +10,5 @@ from chartmetric.tasks import load_chartmetric_stats
 class Command(BaseCommand):
     help = 'Loads Chartmetrics IDs for Tracks and Artists based on ISRCs'
     def handle(self, *args, **options):
-        cm = Chartmetric()
-        cm.authenticate()
-
         for artist in Artist.objects.exclude(chartmetric_id='').filter(spotify_followers=0).order_by('?'):
             load_chartmetric_stats.delay(artist.id)
