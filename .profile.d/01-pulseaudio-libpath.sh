7d63e89fab0d0d4e91231ba0cff95454806d5aee
Martin Enrique Chilpa Navarro <martin@Martins-MacBook-Air.local>
2026-04-04 20:05:01 -0600

    Adding libblapack3

--- .profile.d/01-pulseaudio-libpath.sh
+++ .profile.d/01-pulseaudio-libpath.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-# ffmpeg (installed via heroku-buildpack-apt) can be linked against PulseAudio.
-# On Ubuntu Jammy the `libpulsecommon-15.99.so` library lives in a non-default
-# directory, so we add it to the dynamic loader search path at runtime.
+APT_LIB_DIR="/app/.apt/usr/lib/x86_64-linux-gnu"
 PULSE_LIB_DIR="/app/.apt/usr/lib/x86_64-linux-gnu/pulseaudio"
 
-if [ -d "$PULSE_LIB_DIR" ]; then
-  export LD_LIBRARY_PATH="${PULSE_LIB_DIR}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
-fi
+export LD_LIBRARY_PATH="${APT_LIB_DIR}:${PULSE_LIB_DIR}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
